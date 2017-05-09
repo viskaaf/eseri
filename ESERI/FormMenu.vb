1052902608d65ef33a791288a5cdcaef161f19fe
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2017-05-09 19:41:14 +0700

    Remove debugging

--- ESERI/FormMenu.vb
+++ ESERI/FormMenu.vb
@@ -17,9 +17,4 @@
         FormLogin.TBPassword.Text = ""
         FormLogin.TBUsername.Focus()
     End Sub
-
-    Private Sub FormMenu_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim lala As Double = hitungUmur(FormLogin.userDate)
-        MsgBox(FormLogin.userDate + " " + Today.Date.ToString + " " + lala.ToString + " " + waktuDuduk.ToString)
-    End Sub
 End Class
